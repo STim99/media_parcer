d9d242693615fba64b816d91b33a9743cd3d3843
Tim <tymofii.sankov@wtt-solutions.com>
2022-02-08 18:55:50 +0200

    feat decode link

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { Buffer } from "buffer";
 import { gotScraping } from "got-scraping";
 import { resolve } from "path";
 
@@ -12,6 +13,41 @@ import ejs from "ejs";
 
 const fastify = Fastify({ logger: true });
 const PORT = process.env.PORT ?? 3000;
+
+function decode(b) {
+  function c(a) {
+    return Buffer.from(a.toString("base64"));
+  }
+  function d(a) {
+    return decodeURIComponent(
+      Buffer.from(a, "base64")
+        .toString("binary")
+        .split("")
+        .map(function (a) {
+          return "%" + ("00" + a.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join("")
+    );
+  }
+  const e = {
+    bk0: "$$#!!@#!@##",
+    bk1: "^^^!@##!!##",
+    bk2: "####^!!##!@@",
+    bk3: "@@@@@!##!^^^",
+    bk4: "$$!!@$$@^!@#$$@",
+    file3_separator: "//_//",
+  };
+  let f = b.substr(2);
+  for (let a = 4; -1 < a; a--) {
+    f = f.replace(e.file3_separator + c(e["bk" + a]), "");
+  }
+  try {
+    f = d(f);
+  } catch (a) {
+    f = "";
+  }
+  return f;
+}
 
 fastify.register(formBodyPlugin);
 fastify.register(fastifyStatic, {
@@ -37,15 +73,18 @@ fastify.post("/api", async (request, reply) => {
   request.log.info(request.body);
   gotScraping({
     url: request.body.url,
+    headers: {
+      referer: "https://rezka.ag/",
+    },
     headerGeneratorOptions: {
       browsers: [
         {
           name: "chrome",
-          minVersion: 87,
+          minVersion: 90,
         },
         {
           name: "firefox",
-          minVersion: 80,
+          minVersion: 85,
         },
       ],
       devices: ["desktop", "mobile"],
@@ -57,13 +96,18 @@ fastify.post("/api", async (request, reply) => {
       request.log.info({ statusCode: response.statusCode, url: response.url });
       request.log.info({ body: response.body });
       const url = [];
-      const media = response.body.match(/\[(2|3).+mp4/gi)?.[0];
+      const findHash = response.body
+        .match(/"streams":"#[a-zA-Z0-9\\/_=]*"/gi)?.[0]
+        .replace(/\\/g, "")
+        .split('"')?.[3];
+      const media = decode(findHash);
       if (media) {
-        media.split(/,| or /).forEach((link) => {
+        const links = media.split(/,| or /);
+        for (const link of links) {
           if (/^http/i.test(link)) {
             url.push(normalizeUrl(new URL(link).href));
           }
-        });
+        }
         return reply.view("/templates/index.min.ejs", { url });
       } else {
         return reply.status(500).send("Media is not found");
